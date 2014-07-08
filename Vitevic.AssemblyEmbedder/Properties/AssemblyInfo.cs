e365303764281e115cca30e7101106e1964ad6f2
Vitalij Shitts <tabellarius@gmail.com>
2014-07-08 12:39:15 +0400

    RC 1.0.1

--- Vitevic.AssemblyEmbedder/Properties/AssemblyInfo.cs
+++ Vitevic.AssemblyEmbedder/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vitevic.AssemblyEmbedder")]
+[assembly: AssemblyTitle("Vitevic Assembly Embedder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vitevic Software")]
-[assembly: AssemblyProduct("Vitevic.AssemblyEmbedder")]
+[assembly: AssemblyProduct("Vitevic Assembly Embedder")]
 [assembly: AssemblyCopyright("Copyright © Vitevic Software 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 #if !DEBUG
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
