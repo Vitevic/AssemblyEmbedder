d8dfe7ef7cdf8b060c4aca8388301df196bd3c30
Vitalij Shitts <tabellarius@gmail.com>
2015-07-28 00:35:18 +0300

    VS 2015 Support

---
+++ Vitevic.AssemblyEmbedder.InjectTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vitevic.AssemblyEmbedder.InjectTest
+{
+    class Program
+    {
+        private static Dictionary<string, System.Reflection.Assembly> fields = new Dictionary<string, System.Reflection.Assembly>();
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
