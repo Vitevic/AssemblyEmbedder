884f571fab94e35cde456dc7c694c7f60bb69b1b
Vitalij Shitts <vs@vitevic.com>
2019-08-14 19:00:53 +0300

    fixed warnings for VS2017 build

--- MsBuild/Vitevic.AssemblyEmbedder.MsBuild/Attributes.cs
+++ MsBuild/Vitevic.AssemblyEmbedder.MsBuild/Attributes.cs
@@ -1,10 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Build.Framework;
-using Microsoft.Build;
-using Microsoft.Build.Utilities;
 
 namespace Vitevic.AssemblyEmbedder.MsBuild
 {
