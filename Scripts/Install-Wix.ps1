2f6c413954fff89c504074e005f8fd8c139bdc83
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2020-05-04 13:37:50 -0700

    Proper installation as executable

--- Scripts/Install-Wix.ps1
+++ Scripts/Install-Wix.ps1
@@ -4,9 +4,11 @@
 ##  Desc:  Install Latest Wix
 ################################################################################
 
-Import-Module $PSScriptRoot\ImageHelpers\InstallHelpers.ps1
+echo $PSScriptRoot
+
+Import-Module -Name $PSScriptRoot\ImageHelpers -Verbose
 
 $temp_install_dir = 'C:\Windows\Installer'
 New-Item -Path $temp_install_dir -ItemType Directory -Force
 
-Install-MSI -MsiUrl "https://wixtoolset.org/downloads/v3.14.0.3910/wix314.exe" -MsiName "wix314.msi"
+Install-EXE -Url "https://wixtoolset.org/downloads/v3.14.0.3910/wix314.exe" -Name "wix314.exe" -ArgumentList "/quiet"
