e09a534b893ce72f295abd5fee4381556ac56154
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-01-19 09:09:24 -0800

    Updated script to install WIX v3.14.0.4118

--- Scripts/Install-Wix.ps1
+++ Scripts/Install-Wix.ps1
@@ -4,11 +4,11 @@
 ##  Desc:  Install Latest Wix
 ################################################################################
 
-echo $PSScriptRoot
+Write-Output ("Script root: '{0}'" -f $PSScriptRoot)
 
-Import-Module -Name $PSScriptRoot\ImageHelpers -Verbose
+Import-Module -Name "$PSScriptRoot\ImageHelpers" -Verbose
 
 $temp_install_dir = 'C:\Windows\Installer'
 New-Item -Path $temp_install_dir -ItemType Directory -Force
 
-Install-EXE -Url "https://wixtoolset.org/downloads/v3.14.0.3910/wix314.exe" -Name "wix314.exe" -ArgumentList "/quiet"
+Install-EXE -Url "https://wixtoolset.org/downloads/v3.14.0.4118/wix314.exe" -Name "wix314.exe" -ArgumentList "/quiet"
